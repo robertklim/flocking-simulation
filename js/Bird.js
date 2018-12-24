df88cee2c7a61db62a12483fb1cbce1f103652d6
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-24 21:50:41 +0100

    Add cohesion (steer towards average position of neighbours (long range attraction))

--- js/Bird.js
+++ js/Bird.js
@@ -21,6 +21,7 @@ class Bird {
         }
     }
 
+    // steer towards average heading of neighbours
     align(birds) {
         // align only with birds that you can see
         let perceptionRadius = 50;
@@ -48,16 +49,49 @@ class Bird {
         return steering;
     }
 
+    // steer towards average position of neighbours(long range attraction)
+    cohesion(birds) {
+        let perceptionRadius = 50;
+        let steering = createVector();
+        let total = 0;
+        for (let bird of birds) {
+            let d = dist(this.position.x, this.position.y, bird.position.x, bird.position.y);
+            if (d < perceptionRadius && bird !== this) {
+                steering.add(bird.position);
+                total++;
+            }
+        }
+        if (total > 0) {
+            // get average neighbors velocity
+            steering.div(total);
+            steering.sub(this.position);
+            // set speed
+            steering.setMag(this.maxSpeed);
+            // calculate steering force
+            steering.sub(this.velocity);
+            // limit steering force
+            steering.limit(this.maxForce);
+        }
+        // return steering force
+        return steering;
+    }
+
     flock(birds) {
         // calculate alignment force
         let alignment = this.align(birds);
-        // apply alignment
-        this.acceleration = alignment;;
+        // calculate cohesion force
+        let cohesion = this.cohesion(birds);
+        // use add to apply both forces
+        this.acceleration.add(alignment);
+        this.acceleration.add(cohesion);
     }
 
     update() {
         this.position.add(this.velocity);
         this.velocity.add(this.acceleration);
+        this.velocity.limit(this.maxSpeed);
+        // reset acceleration
+        this.acceleration.set(0, 0);
     }
 
     show() {
